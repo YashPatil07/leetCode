16d1ac6bb2207d1947b548e6594b47539fbbfa21
Yash Patil <91176651+YashPatil07@users.noreply.github.com>
2024-09-08 18:50:43 +0530

    Time: 964 ms (24.18%), Space: 20.7 MB (31.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,17 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         st = set()
+        nums.sort()
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                     continue 
+                    
             hashmap=set()
+            
             for j in range(i+1,len(nums)):
                 third = -(nums[i]+nums[j])
                 # rem = target - third
                 if third in hashmap:    
                     temp =[nums[i],nums[j],third]
-                    temp.sort()
                     st.add(tuple(temp))
                 hashmap.add(nums[j])             
+                
         ans = list(st)
         return ans
