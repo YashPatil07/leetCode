c2363032b8cb7e903f57a7cc4a31ed1cc56928ca
Yash Patil <91176651+YashPatil07@users.noreply.github.com>
2024-09-08 23:09:47 +0530

    Time: 695 ms (60.37%), Space: 20.7 MB (72.45%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,20 +1,78 @@
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        st = set()
+        ans=[]
         nums.sort()
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                    continue 
+        n=len(nums)
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+             
+            j = i + 1
+            k = n - 1
+
+            while j < k:
+                total = nums[i]+nums[j]+nums[k]
+
+                if total < 0:
+                    j+=1
+                elif total > 0:
+                    k-=1
+                else:
+                    temp=[nums[i],nums[j],nums[k]]
+                    ans.append(temp)
+                    j+=1
+                    k-=1
+
+                    while j < k and nums[j] == nums[j-1]:
+                        j+=1 
                     
-            hashmap=set()
+                    while j < k and nums[k] == nums[k+1]:
+                        k-=1
+        return ans    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #time complexity and space complexity is o(n^2)
+#         st = set()
+#         nums.sort()
+#         for i in range(len(nums)):
+#             if i > 0 and nums[i] == nums[i - 1]:
+#                     continue 
+                    
+#             hashmap=set()
             
-            for j in range(i+1,len(nums)):
-                third = -(nums[i]+nums[j])
-                # rem = target - third
-                if third in hashmap:    
-                    temp =[nums[i],nums[j],third]
-                    st.add(tuple(temp))
-                hashmap.add(nums[j])             
+#             for j in range(i+1,len(nums)):
+#                 third = -(nums[i]+nums[j])
+#                 # rem = target - third
+#                 if third in hashmap:    
+#                     temp =[nums[i],nums[j],third]
+#                     st.add(tuple(temp))
+#                 hashmap.add(nums[j])             
                 
-        ans = list(st)
-        return ans
+#         ans = list(st)
+#         return ans
+
+    #above code need more space 
+    
